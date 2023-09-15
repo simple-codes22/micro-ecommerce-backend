29289d8148d0cab1cd769a97c09efc3a21c0cea3
Simeon Akanbi <simeonakanbi99@gmail.com>
2023-09-16 00:56:15 +0100

    Finished commenting and formatting python modules

--- core/serializers.py
+++ core/serializers.py
@@ -3,24 +3,29 @@ from .models import *
 from django.contrib.auth import get_user_model
 
 
+# Serializer for the Product model
 class ProductSerializers(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = "__all__"  # Include all fields in the serialized representation
 
+
+# Serializer for the ProductImage model
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
-        fields = "__all__"
+        fields = "__all__"  # Include all fields in the serialized representation
 
+
+# Serializer for the User model (custom user model)
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = "__all__"
+        model = get_user_model()  # Get the custom user model
+        fields = "__all__"  # Include all fields in the serialized representation
 
+
+# Serializer for the Order model
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = "__all__"
-
-
+        fields = "__all__"  # Include all fields in the serialized representation
