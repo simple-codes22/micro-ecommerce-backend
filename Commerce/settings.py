582cc4db6570b6b0a0bc096719dd649e7b2c65df
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-01-10 01:26:32 +0100

    Changed token lifetime for access and refresh

--- Commerce/settings.py
+++ Commerce/settings.py
@@ -78,8 +78,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=8),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=40),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
